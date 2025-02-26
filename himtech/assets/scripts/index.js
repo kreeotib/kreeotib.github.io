542c9591078023d34d23a3fa377b81eb05567b86
kreotib <kreotib@gmail.com>
2025-02-26 14:43:49 +0300

    1234

--- himtech/assets/scripts/index.js
+++ himtech/assets/scripts/index.js
@@ -1,4 +1,37 @@
+const initTabs = () => {
+    const tabs = [...document.querySelectorAll(".tabs")];
+
+    if (tabs.length > 0) {
+        tabs.forEach((tab) => {
+            const tabContent = [...tab.querySelectorAll(".tabs__content")];
+            const tabLinks = [...tab.querySelectorAll(".tabs__link")];
+
+            const openTab = (tabIndex = 0) => {
+                tabContent.forEach((element, i) => {
+                    const isActive = i === tabIndex;
+                    element.classList.toggle("active", isActive);
+                });
+
+                tabLinks.forEach((element, i) => {
+                    element.classList.toggle("active", i === tabIndex);
+                });
+            }
+
+            openTab(0)
+
+            tabLinks.forEach((link, i) => {
+                link.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    openTab(i);
+                });
+            });
+        });
+    }
+}
+
 document.addEventListener('DOMContentLoaded',()=>{
+    initTabs();
+
     const documentsSlider = new Swiper('.documents-slider', {
         slidesPerView: 'auto',
         spaceBetween:4,
