de6f4400488377211d26e37e34402f2a730e6599
kreotib <kreotib@gmail.com>
2025-04-24 19:23:20 +0300

    1234

--- tour-barcelona/assets/scripts/index.js
+++ tour-barcelona/assets/scripts/index.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const burger = document.querySelector('.burger'),
         headerNav = document.querySelector('.header-nav');
 
-    if(burger && headerNav){
-        burger.addEventListener('click',e=>{
+    if (burger && headerNav) {
+        burger.addEventListener('click', e => {
             e.preventDefault();
 
             burger.classList.toggle('burger--active');
@@ -12,20 +12,22 @@ document.addEventListener("DOMContentLoaded",()=>{
     }
 
 
-    const heroVideo = document.querySelector('.hero-video');
+    const videoItemArray = document.querySelectorAll('.video-item');
 
-    if(heroVideo){
-        const heroVideoItem = heroVideo.querySelector('.hero-video__item video'),
-            heroVideoMute = heroVideo.querySelector('.hero-video__mute'),
-            heroVideoText = heroVideoMute.querySelector("span");
+    if (videoItemArray.length) {
+        videoItemArray.forEach(video => {
+            const videoElement = video.querySelector('.video-item__element video'),
+                videoMute = video.querySelector('.video-item__mute');
 
-        heroVideoMute.classList.toggle('unmuted', !heroVideoItem.muted)
+            if (!videoElement && !videoMute) return false;
 
-        heroVideoMute.addEventListener('click',e=>{
-            e.preventDefault();
+            videoMute.classList.toggle('unmuted', !videoElement.muted)
+            videoMute.addEventListener('click', e => {
+                e.preventDefault();
 
-            heroVideoItem.muted = !heroVideoItem.muted;
-        heroVideoMute.classList.toggle('unmuted', !heroVideoItem.muted)
+                videoElement.muted = !videoElement.muted;
+                videoMute.classList.toggle('unmuted', !videoElement.muted)
+            })
         })
     }
 })
