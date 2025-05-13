f34dc2b3b413b2d3c736971c2460bb4f87fddd66
kreotib <kreotib@gmail.com>
2025-05-13 13:41:56 +0300

    12345

--- brandshop/assets/scripts/index.js
+++ brandshop/assets/scripts/index.js
@@ -32,12 +32,116 @@ class Animations {
     }
 }
 
+const checkTargetOrKey = event => {
+    if (
+        event.target.classList.contains('popup__wrapper') ||
+        event.key === 'Escape' ||
+        event.target.closest('.popup-close')
+    ) {
+        hideAllPopups();
+    }
+};
+const showPopup = popupId => {
+    const popup = document.querySelector(popupId);
+    if (!popup) return
+
+
+    hideAllPopups();
+
+    popup.classList.add('popup--active');
+    document.body.classList.add('no-scroll');
+
+    document.addEventListener('click', checkTargetOrKey);
+    document.addEventListener('keyup', checkTargetOrKey);
+};
+const hideAllPopups = () => {
+    const popups = document.querySelectorAll('.popup');
+
+    popups.forEach(popup => {
+        popup.classList.remove('popup--active');
+    });
+    document.body.classList.remove('no-scroll');
+
+    document.removeEventListener('click', checkTargetOrKey);
+    document.removeEventListener('keyup', checkTargetOrKey);
+};
+
 
 document.addEventListener("DOMContentLoaded", () => {
+    const popupButtons = document.querySelectorAll('[data-popup]');
+    const popups = document.querySelectorAll('.popup');
+
+    if (popups.length) {
+        popupButtons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const popupId = button.dataset.popup
+                showPopup(popupId);
+            });
+        });
+    }
+
     const animation = new Animations();
     animation.init();
 
     const lenis = new Lenis({
-        autoRaf: true,
+        anchors: {
+            offset: -document.querySelector('.header').getBoundingClientRect().height,
+            onClick: ()=>{
+                console.log('scrolled to anchor')
+            }
+        },
     });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    const burger = document.querySelector('.burger'),
+        menu = document.querySelector('.menu'),
+        header = document.querySelector('.header');
+
+    if(burger && menu){
+        burger.addEventListener('click',e=>{
+            e.preventDefault();
+
+            header.classList.toggle('header--active');
+            burger.classList.toggle('burger--active');
+            menu.classList.toggle('menu--active');
+            header.classList.add('sticky');
+
+            menu.classList.contains('menu--active') ? lenis.stop() : lenis.start();
+        })
+    }
+
+    if(menu){
+        const anchorLinks = document.querySelectorAll('a');
+
+        if (anchorLinks.length) {
+            anchorLinks.forEach(link => {
+                if (link.hash.length > 0) {
+                    link.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        burger.classList.remove('burger--active');
+                        menu.classList.remove('menu--active');
+                        header.classList.remove('header--active');
+                        lenis.start();
+                    })
+                }
+            });
+        }
+    }
+
+    const stickyElm = document.querySelector('.header')
+
+    const observer = new IntersectionObserver(
+        ([e]) => e.target.classList.toggle('sticky', e.intersectionRatio < 1),
+        {threshold: [1]}
+    );
+
+    observer.observe(stickyElm)
 })
