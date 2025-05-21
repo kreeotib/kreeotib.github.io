382c7bf06f74146a7ba66a52b0989cc5abe389b8
kreotib <kreotib@gmail.com>
2025-05-21 17:33:05 +0300

    1234

--- brandshop/assets/scripts/index.js
+++ brandshop/assets/scripts/index.js
@@ -153,7 +153,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
         await ymaps3.ready;
 
-        const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+        const {YMap, YMapDefaultSchemeLayer, YMapMarker} = ymaps3;
 
         // Иницилиазируем карту
         const map = new YMap(
