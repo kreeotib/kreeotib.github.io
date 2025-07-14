260883bde193b90085bdf79b85f1b1c524268656
kreotib <sonofzeuc@gmail.com>
2025-07-14 21:16:03 +0300

    1234

--- zayavka-zakrita/assets/scripts/index.js
+++ zayavka-zakrita/assets/scripts/index.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded',e=>{
+document.addEventListener('DOMContentLoaded', e => {
     const burger = document.querySelector('.burger'),
         menu = document.querySelector('.menu'),
         menuClose = document.querySelector('.menu__close');
 
-    if(burger && menu && menuClose){
-        burger.addEventListener('click',e=>{
+    if (burger && menu && menuClose) {
+        burger.addEventListener('click', e => {
             e.preventDefault();
 
             menu.classList.toggle('active');
             document.body.classList.toggle('no-scroll')
         })
-        menuClose.addEventListener('click',e=>{
+        menuClose.addEventListener('click', e => {
             e.preventDefault();
 
             menu.classList.toggle('active');
@@ -21,9 +21,22 @@ document.addEventListener('DOMContentLoaded',e=>{
     const stickyElm = document.querySelector('.header')
 
     const observer = new IntersectionObserver(
-        ([e]) => {e.target.classList.toggle('sticky', e.intersectionRatio < 1), document.body.classList.toggle('header-active',e.intersectionRatio < 1)},
+        ([e]) => {
+            e.target.classList.toggle('sticky', e.intersectionRatio < 1), document.body.classList.toggle('header-active', e.intersectionRatio < 1)
+        },
         {threshold: [1]}
     );
 
     observer.observe(stickyElm);
+
+    let lastScrollTop = 0;
+    window.addEventListener("scroll", function () {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            stickyElm.classList.add('hidden')
+        } else {
+            stickyElm.classList.remove('hidden')
+        }
+        lastScrollTop = scrollTop;
+    });
 })
